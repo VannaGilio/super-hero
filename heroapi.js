9eeb6c3be5ff65c9898b5e2c62bf042bfa284076
GIOVANNA COELHO GILIO DE SA <24122381@CFP127ALUNO.LOCAL>
2025-03-11 12:23:30 -0300

    criando card

--- heroapi.js
+++ heroapi.js
@@ -1,27 +1,28 @@
 'use strict'
 
 async function pesquisarHeroi(pesquisarHeroi) {
-    const url = `https://www.superheroapi.com/api.php/6ca719a09e727c39f05602c88a26d21b/search/${pesquisarHeroi}`
-    const response = await fetch(url)
-    const date = await response.json()
+    const accessToken = `6ca719a09e727c39f05602c88a26d21b`
+    const url = `https://www.superheroapi.com/api.php/${accessToken}/search/${pesquisarHeroi}`
+    
+    const response = await fetch(url) 
+    const data = await response.json()
 
-    return date
+    return data
+    
 }
 
 function criarImagem(link){
     const imagemHeroi = document.getElementById('imagem')
     const novaImagemHeroi = document.createElement('img')
-    novaImg.src = link
-
+    novaImagemHeroi.src = link
     imagemHeroi.appendChild(novaImagemHeroi)
 }
 
 async function dadosHeroi() {
     const heroi = document.getElementById('pesquisaHeroi').value
     const fotoHeroi = await pesquisarHeroi(heroi)
-    const imagemHeroi = document.getElementById('imagem')
-    imagemHeroi.replaceChildren('')
-
+    const imagemHeroi3 = document.getElementById('imagem')
+    imagemHeroi3.replaceChildren('')
 
     console.log(fotoHeroi)
 }
