cfba306ab85ff033048e5cea10f68d2f74b1302c
Stella <07gilio@gmail.com>
2025-03-24 21:54:13 -0300

    caracteristicas retornando ok

--- heroapi.js
+++ heroapi.js
@@ -7,7 +7,7 @@ const pesquisarImagem = async (text) => {
     return response.json()
 }
 
-const createCard = ({image, name, appearance}) =>{
+const createCard = ({image, name, appearance, powerstats}) =>{
     const card = document.createElement('div')
     card.classList.add('section')
     
@@ -19,7 +19,16 @@ const createCard = ({image, name, appearance}) =>{
                 ${name}
             </div>
             <div class="caracteristicas">
-                <h3>POWERSTATS</h3>
+                <h3><a href="#" class="click-powerstats">POWERSTATS</a></h3>
+                <ul class = "lista-powerstats">
+                    <li>INTELLIGENCE: ${powerstats.intelligence} </li>
+                    <li>STRENGTH: </li>
+                    <li>SPEED: </li>
+                    <li>DURABILITY: </li>
+                    <li>POWER: </li>
+                    <li>COMBAT: </li>
+                </ul>
+
                 <h3>BIOGRAPHY</h3>
                 <h3>APPEARANCE</h3>
                 <h3>CONNECTIONS</h3>
@@ -28,16 +37,27 @@ const createCard = ({image, name, appearance}) =>{
         </div>
     </div>
     `
+
+    //Clicar e aparecer as caractwristicas
+    const clickPowerstats = card.querySelector('.click-powerstats')
+    const listaPowerstats = card.querySelector('.lista-powerstats')
+
+    clickPowerstats.addEventListener('click', (event) => {
+        event.preventDefault()  //"muda" a direção que o href teria, pq ele é um link e a API não
+        listaPowerstats.style.display = listaPowerstats.style.display === 'none' ? 'block' : 'none' 
+    })
+
+
     return card
 }
 
 //Trás as informações e criação do card
 const pesquisarHeroi = async (text) => {
-    const container = document.querySelector('.conteudo')
+    const container = document.querySelector('.container')
     const {results} = await pesquisarImagem(text)
     const cards = results.map(createCard)
     container.replaceChildren(...cards)
-    document.querySelector('#pesquisar-heroi').velue = text
+    document.querySelector('#pesquisa-heroi').value = text
 }
 
 // Ao presionar a tecla enter pegar o valor da função pesquisar
