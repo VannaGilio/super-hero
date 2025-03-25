84b6fcc677b095b3fecad1626be4f802b041426b
Stella <07gilio@gmail.com>
2025-03-25 00:02:23 -0300

    trabalho finalizado

--- heroapi.js
+++ heroapi.js
@@ -7,7 +7,7 @@ const pesquisarImagem = async (text) => {
     return response.json()
 }
 
-const createCard = ({image, name, appearance, powerstats}) =>{
+const createCard = ({image, name, appearance, powerstats, biography, connections, work}) =>{
     const card = document.createElement('div')
     card.classList.add('section')
     
@@ -18,35 +18,115 @@ const createCard = ({image, name, appearance, powerstats}) =>{
             <div class="nome-heroi">
                 ${name}
             </div>
+
             <div class="caracteristicas">
                 <h3><a href="#" class="click-powerstats">POWERSTATS</a></h3>
-                <ul class = "lista-powerstats">
-                    <li>INTELLIGENCE: ${powerstats.intelligence} </li>
-                    <li>STRENGTH: </li>
-                    <li>SPEED: </li>
-                    <li>DURABILITY: </li>
-                    <li>POWER: </li>
-                    <li>COMBAT: </li>
-                </ul>
+                <h3><a href="#" class="click-biography">BIOGRAPHY</a></h3>
+                <h3><a href="#" class="click-appearance">APPEARANCE</a></h3>
+                <h3><a href="#" class="click-connections">CONNECTIONS</a></h3>
+                <h3><a href="#" class="click-work">WORK</a></h3>
+            </div>
 
-                <h3>BIOGRAPHY</h3>
-                <h3>APPEARANCE</h3>
-                <h3>CONNECTIONS</h3>
-                <h3>WORK</h3>
+            <div class="listas">
+                <div class="lista">
+                    <ul class = "lista-powerstats">
+                        <li>INTELLIGENCE:   ${powerstats.intelligence} </li>
+                        <li>STRENGTH:   ${powerstats.strength}</li>
+                        <li>SPEED:   ${powerstats.speed}</li>
+                        <li>DURABILITY:   ${powerstats.durability}</li>
+                        <li>POWER:   ${powerstats.power}</li>
+                        <li>COMBAT:   ${powerstats.combat}</li>
+                    </ul>
+                </div>
+
+                <div class="lista">
+                    <ul class = "lista-biography">
+                        <li>FULL NAME:   ${biography["full-name"]}</li>
+                        <li>ALTER-EGOS:   ${biography["alter-egos"]}</li>
+                        <li>ALIASES:   ${biography["aliases"]}</li>
+                        <li>PLACE OF BIRTH:   ${biography["place-of-birth"]}</li>
+                        <li>FIRST APPEARANCE:   ${biography["first-appearance"]}</li>
+                        <li>ALIGNMENT:   ${biography["alignment"]}</li>
+                    </ul>
+                </div>
+
+                <div class="lista">
+                    <ul class = "lista-appearance">
+                        <li>GENDER:   ${appearance.gender}</li>
+                        <li>RACE:   ${appearance.race}</li>
+                        <li>HEIGHT:   ${appearance.height}</li>
+                        <li>WEIGHT:   ${appearance.weight}</li>
+                        <li>EYE-COLOR:   ${appearance["eye-color"]}</li>
+                        <li>HAIR-COLOR:   ${appearance["hair-color"]}</li>
+                    </ul>
+                </div>
+
+                <div class="lista">
+                    <ul class = "lista-connections">
+                        <li>GROUP-AFFILIATION:   ${connections["group-affiliation"]}</li>
+                        <li>RELATIVES:   ${connections.relatives}</li>
+                    </ul>
+                </div>
+                
+                <div class="lista">
+                    <ul class = "lista-work">
+                        <li>OCCUPATION:   ${work.occupation}</li>
+                        <li>BASE:   ${work.base}</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
     `
 
+    const esconderCaracteristicas = () => {
+        listaPowerstats.style.display = 'none'
+        listaBiography.style.display = 'none'
+        listaAppearance.style.display = 'none'
+        listaConnections.style.display = 'none'
+        listaWork.style.display = 'none'
+    }
+
     //Clicar e aparecer as caractwristicas
     const clickPowerstats = card.querySelector('.click-powerstats')
     const listaPowerstats = card.querySelector('.lista-powerstats')
-
-    clickPowerstats.addEventListener('click', (event) => {
+    clickPowerstats.addEventListener('click', (event) => { //perguntar para o prof se tem como fazer isso como uma "repetição"
         event.preventDefault()  //"muda" a direção que o href teria, pq ele é um link e a API não
-        listaPowerstats.style.display = listaPowerstats.style.display === 'none' ? 'block' : 'none' 
+        esconderCaracteristicas() //"esconder" a caracrteristica anterior
+        listaPowerstats.style.display = listaPowerstats.style.display === 'none' ? 'block' : 'none' //não aparecer direto na tela após pesquisa
     })
 
+    const clickBiography = card.querySelector('.click-biography')
+    const listaBiography = card.querySelector('.lista-biography')
+    clickBiography.addEventListener('click', (event) => { 
+        event.preventDefault()
+        esconderCaracteristicas()
+        listaBiography.style.display = listaBiography.style.display === 'none' ? 'block' : 'none' 
+    })
+
+    const clickAppearance = card.querySelector('.click-appearance')
+    const listaAppearance = card.querySelector('.lista-appearance')
+    clickAppearance.addEventListener('click', (event) => { 
+        event.preventDefault()
+        esconderCaracteristicas()
+        listaAppearance.style.display = listaAppearance.style.display === 'none' ? 'block' : 'none' 
+    })
+
+    const clickConnections = card.querySelector('.click-connections')
+    const listaConnections = card.querySelector('.lista-connections')
+    clickConnections.addEventListener('click', (event) => { 
+        event.preventDefault()
+        esconderCaracteristicas()
+        listaConnections.style.display = listaConnections.style.display === 'none' ? 'block' : 'none' 
+    })
+
+    const clickWork = card.querySelector('.click-work')
+    const listaWork = card.querySelector('.lista-work')
+    clickWork.addEventListener('click', (event) => { 
+        event.preventDefault()
+        esconderCaracteristicas()
+        listaWork.style.display = listaWork.style.display === 'none' ? 'block' : 'none' 
+    })
 
     return card
 }
@@ -55,9 +135,14 @@ const createCard = ({image, name, appearance, powerstats}) =>{
 const pesquisarHeroi = async (text) => {
     const container = document.querySelector('.container')
     const {results} = await pesquisarImagem(text)
-    const cards = results.map(createCard)
-    container.replaceChildren(...cards)
-    document.querySelector('#pesquisa-heroi').value = text
+    if(results){
+        const cards = results.map(createCard)
+        container.replaceChildren(...cards)
+    }else{
+        alert("Herói não encontrado!")
+    }
+    
+    document.querySelector('#pesquisa-heroi').value = ''
 }
 
 // Ao presionar a tecla enter pegar o valor da função pesquisar
